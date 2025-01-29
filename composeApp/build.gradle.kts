728589f1b0afaf76541fb260622d3877f7757a73
Shyak Das <shayak.das007@gmail.com>
2025-01-29 14:19:12 +0530

    Added splash sceen

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -58,6 +58,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.core.splashscreen)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
--- composeApp/src/androidMain/kotlin/org/trackmyexpenses/project/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/trackmyexpenses/project/MainActivity.kt
@@ -5,11 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                true
+            }
+        }
         setContent {
             App()
         }
